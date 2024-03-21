b4ced8aa1b62688dfe7744fdefb87017965d2405
lena223 <lelenahkht2003@gamil.com>
2024-03-21 15:05:47 +0700

    :hammer: setup dev environment

--- server.js
+++ server.js
@@ -1,5 +1,13 @@
+const { config } = require("dotenv");
 const express = require("express");
+const morgan = require("morgan");
 const app = express();
+config();
+const port = process.env.PORT;
+
+if (process.env.NODE_ENV === "development") {
+  app.use(morgan("dev"));
+}
 
 app.listen(port, () => {
   console.log(`Server is running on ${port} `);
